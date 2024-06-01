a9301dfdd311b80bb20cf87c1eed4f5217936479
Christopher Monterroso <chrismonc08@gmail.com>
2024-05-31 23:31:58 -0600

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route
             path="/dashboard/*"
